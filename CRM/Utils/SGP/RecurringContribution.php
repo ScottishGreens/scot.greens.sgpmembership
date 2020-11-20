fd649fb27b42799b8c4de580c95d8c0c2eeb1ea6
Leckie <sle4438>
2020-11-20 14:23:44 +0000

    when setting RC transaction ID get the latest linked contribution

--- CRM/Utils/SGP/RecurringContribution.php
+++ CRM/Utils/SGP/RecurringContribution.php
@@ -211,11 +211,25 @@
             return;
         }
 
+        // Fetch RC
+        $contribrecur_get = civicrm_api3('ContributionRecur', 'get', array(
+            'sequential' => 1,
+            'return' => ['frequency_unit', 'amount', 'contact_id', 'id', 'frequency_interval', 'trxn_id'],
+            'id' => $recurring_contribution_id
+        ) );
+
+        if (!empty($contribrecur_get['values'][0]['trxn_id'])) {
+            Civi::log()->debug("Transaction ID already exists {$contribrecur_get['values'][0]['trxn_id']}");
+            return;
+        }
+
+        // Get latest linked payment
         $contribs = civicrm_api3('Contribution', 'get', [
           'sequential' => 1,
           'options' => ['limit' => 1],
           'return' => ['id',$txn_custom_field],
           'contribution_recur_id' => $recurring_contribution_id,
+          'options' => ['sort' => "receive_date DESC"],
         ]); 
 
         // If neither exist, we bounce
@@ -226,16 +240,13 @@
 
         $recurring_transaction_id = $contribs['values'][0][$txn_custom_field];
 
+        $recurr_create_params = $contribrecur_get['values'][0];
+        $recurr_create_params['trxn_id'] = $recurring_transaction_id,
+
+
         Civi::log()->debug("Set transaction ID to {$recurring_transaction_id}");
 
-        $recurr = civicrm_api3('ContributionRecur', 'create', [
-          'sequential' => 1,
-          'contact_id' => $contribs['values'][0]['contact_id#'],
-          'id' => $recurring_contribution_id,
-          'trxn_id' => $recurring_transaction_id,
-        ]); 
-
-        CRM_Utils_SGP_RecurringContribution::update($recurring_contribution_id);
+        $recurr = civicrm_api3('ContributionRecur', 'create', $recurr_create_params); 
 
         return true;
 
